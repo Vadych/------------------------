61c5620f1ec6ae5112ef19f61a52b8f0396df822
Vadych <sisinv@gmail.com>
2022-05-23 11:56:29 +0300

    fix

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ import os
 
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
-# 
 
 
 # Объект бота
@@ -15,20 +14,22 @@ bot = Bot(token=BOT_TOKEN)
 
 dp = Dispatcher(bot)
 # Включаем логирование, чтобы не пропустить важные сообщения
-logging.basicConfig(filename="bot.log", 
+logging.basicConfig(filename="bot.log",
                     level=logging.INFO,
                     format='%(asctime)s %(message)s'
                     )
 
+
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
     await message.answer('Привет!\nЭтот бот написал @vadych.\nЕдинственное, что я умею - повторять за тобой')
-    
-    
+
+
 # Хэндлер на команду /test1
 @dp.message_handler(commands="test1")
 async def cmd_test1(message: types.Message):
-    logging.info('Комманда \\test1 от %s(%s)', message.from_user.full_name, message.from_user.mention)
+    logging.info('Комманда \\test1 от %s(%s)',
+                 message.from_user.full_name, message.from_user.mention)
     print(message.text)
     await message.reply("Test 1")
 
@@ -39,9 +40,9 @@ async def cmd_all(message: types.Message):
     logging.info('%s %s от %s(%s)',
                  message.content_type,
                  answer,
-                 message.from_user.full_name, 
+                 message.from_user.full_name,
                  message.from_user.mention)
-    await message.answer(answer)
+    await message.answer(answer or 'Мне нечего сказать')
 
 
 if __name__ == "__main__":
